4a327a19219825975de659007db3022fdf23f6f0
davidsisa <sisabuestandavidesteban@gmail.com>
2023-02-13 11:44:21 -0500

    primeravance

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'mensaje ',
+      message: 'Buenas noches por cierto',
+      buttons: ['COMO ASI'],
+    });
+
+    await alert.present();
+  }
+  async presentAlert1() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'mensaje ',
+      message: 'Buenas noches por cierto',
+      buttons: ['COMO ASI'],
+    });
+
+    await alert.present();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
